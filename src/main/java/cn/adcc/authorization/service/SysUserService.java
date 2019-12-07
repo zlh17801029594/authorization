4380276328239616d4fe366924691bb210cd12ef
xiaozhou <17801029594@163.com>
2019-12-07 13:32:21 +0800

    增加注释2

--- src/main/java/cn/adcc/authorization/service/SysUserService.java
+++ src/main/java/cn/adcc/authorization/service/SysUserService.java
@@ -5,9 +5,12 @@ import cn.adcc.authorization.model.SysUser;
 public interface SysUserService extends CurdService<SysUser> {
     void save();
 
+    /*2.个人修改和管理员修改逻辑不一样，考虑(1)从Authentication中取name，(2)传递整个SysUser。*/
     void changePassword(String oldPassword, String newPassword);
 
     boolean userExists(String username);
 
+    void disableUser();
 
+    void lockUser();
 }
